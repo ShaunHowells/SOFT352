9f426345440e1fec502902f4df9e3db91e1437a4
ShaunH <shaun.howells@students.plymouth.ac.uk>
2018-11-17 14:25:38 +0000

    Added initial UI elements for Available Sessions + My Sessions. TODO: Book display. Chat display.

---
+++ WebApp/sessions.js
@@ -0,0 +1,43 @@
+var sessionTabs = angular.module("sessionTabs", []);
+
+sessionTabs.controller("availableSessionsCtrl", function ($scope) {
+    $scope.availableSessions = [{
+            owner: 'Shaun',
+            name: 'My Test Session',
+            id: 1
+        },
+        {
+            owner: 'John',
+            name: "I'm over here!",
+            id: 2
+        }, {
+            owner: 'Shaun',
+            name: 'My Test Session',
+            id: 1
+        },
+        {
+            owner: 'John',
+            name: "I'm over here!",
+            id: 2
+        }
+    ];
+}).controller("mySessionsCtrl", function ($scope) {
+    $scope.mySessions = [{
+            owner: 'Shaun',
+            name: 'My Active Session 1',
+            id: 1
+        },
+        {
+            owner: 'John',
+            name: "My Active Session 2",
+            id: 2
+        }
+    ];
+});
+
+$(document).ready(function(){
+    $(".list-group-item").click(function(event){
+        $(".list-group-item").removeClass("active");
+        $(this).addClass("active");
+    });
+});
