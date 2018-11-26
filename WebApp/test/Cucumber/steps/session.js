59aa8b5a89e0baa957ac3f742ef7e0964912ca90
ShaunH <shaun.howells@students.plymouth.ac.uk>
2018-11-26 20:51:52 +0000

    Testing cucumber tests passing on Jenkins

--- WebApp/test/Cucumber/steps/session.js
+++ WebApp/test/Cucumber/steps/session.js
@@ -1,2 +1,18 @@
 const { Before , Given , When , Then } = require ("cucumber");
 var assert = require("assert");
+
+  Given('today is Sunday', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return true;
+  });
+
+
+  When('I ask whether it\'s Friday yet', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return true;
+  });
+
+  Then('I should be told {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return true;
+  });
