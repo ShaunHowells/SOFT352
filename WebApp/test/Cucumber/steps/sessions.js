1642cb31ca91b8178aa007eab7161bd42ef86790
ShaunH <shaun.howells@students.plymouth.ac.uk>
2018-11-26 21:39:13 +0000

    Ensuring cucumber tests pass to check Jenkins functionality

--- WebApp/test/Cucumber/steps/sessions.js
+++ WebApp/test/Cucumber/steps/sessions.js
@@ -8,15 +8,15 @@ var assert = require("assert");
 
 Given('today is Sunday', function () {
   // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  return 'true';
 });
 
 When('I ask whether it\'s Friday yet', function () {
   // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  return 'true';
 });
 
 Then('I should be told {string}', function (string) {
   // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  return 'true';
 });
