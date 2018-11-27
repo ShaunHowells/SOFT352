050829bb4463931c63db93d07762fe697e8a8986
ShaunH <shaun.howells@students.plymouth.ac.uk>
2018-11-27 16:18:59 +0000

    Added some basic Cucumber tests for sessions

--- WebApp/test/Cucumber/features/sessions.feature
+++ WebApp/test/Cucumber/features/sessions.feature
@@ -1,7 +1,26 @@
-Feature: Is it Friday yet?
-  Everybody wants to know when it's Friday
+Feature: Sessions
+  In order to collaborate with other users
+  Users should be able to view, create, join, and leave sessions
 
-  Scenario: Sunday isn't Friday
-    Given today is Sunday
-    When I ask whether it's Friday yet
-    Then I should be told "Nope"
+  Scenario: Viewing all available session
+    Given there are available sessions
+    When I ask to see all available sessions
+    Then I should be shown the available session
+
+  Scenario: Creating a session
+    When I try to create a session
+    And I have supplied a name
+    And I have chosen a book
+    Then I should be able to create a session
+    
+  Scenario: Joining an available session
+    Given there are available sessions to join
+    When I click join a session
+    Then I should be added to the session
+    And given information about the session
+    
+  Scenario: Leaving a session
+    Given I am in a session
+    When I click to leave the session
+    Then I should removed from the session
+    
--- WebApp/test/Cucumber/steps/sessions.js
+++ WebApp/test/Cucumber/steps/sessions.js
@@ -1,22 +1,87 @@
 const {
-  Before,
-  Given,
-  When,
-  Then
+    Before,
+    Given,
+    When,
+    Then
 } = require("cucumber");
 var assert = require("assert");
 
-Given('today is Sunday', function () {
-  // Write code here that turns the phrase above into concrete actions
-  return 'true';
+/* 1) Scenario: Viewing all available session # WebApp\test\Cucumber\features\sessions.feature:5 */
+Given('there are available sessions', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
 });
 
-When('I ask whether it\'s Friday yet', function () {
-  // Write code here that turns the phrase above into concrete actions
-  return 'true';
+When('I ask to see all available sessions', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+Then('I should be shown the available session', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+/* END 1 */
+
+
+/* 2) Scenario: Creating a session # WebApp\test\Cucumber\features\sessions.feature:10 */
+When('I try to create a session', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
 });
 
-Then('I should be told {string}', function (string) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'true';
+When('I have supplied a name', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
 });
+
+When('I have chosen a book', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('I should be able to create a session', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+/* END 2 */
+
+
+/* 3) Scenario: Joining an available session # WebApp\test\Cucumber\features\sessions.feature:16 */
+Given('there are available sessions to join', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('I click join a session', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('I should be added to the session', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('given information about the session', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+/* END 3 */
+
+
+/*4) Scenario: Leaving a session # WebApp\test\Cucumber\features\sessions.feature:22 */
+Given('I am in a session', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('I click to leave the session', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('I should removed from the session', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+/* END 4 */
